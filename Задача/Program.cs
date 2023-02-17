7a09bf39d2c1ea94e4735dd99aef364cc3cc658a
DariyaSazonova <Sazonovade@gmail.com>
2023-02-17 16:31:58 +0300

    Добавила решение задачи

---
+++ Задача/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] ArrayOne = new string[5] {"Hi","World","pass","computer","456"};
+string[] ArrayTwo = new string[ArrayOne.Length];
+
+
+void Sort(string[] ArrayOne, string[] ArrayTwo)
+{
+    int count = 0;
+    for( int i = 0; i< ArrayOne.Length; i++)
+    {
+        if(ArrayOne[i].Length <= 3)
+        {
+            ArrayTwo[count] = ArrayOne[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i< array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Sort(ArrayOne, ArrayTwo);
+PrintArray(ArrayTwo);
